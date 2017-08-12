019ffdbf9feae1b9860b5d0d5aa20d8e3603f3ec
AnonWolf99 <pindian09@gmail.com>
2017-08-12 21:12:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one', function(req,res) {
 });
 
 app.get('/article-two', function(req,res) {
-    res.send('Ärticle TWO requested and will be given here.');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function(req,res) {
