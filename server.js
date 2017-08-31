3631077b217610c4bfe19529e8aa5209a7da9905
AnonWolf99 <pindian09@gmail.com>
2017-08-31 19:14:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var app = express();
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'pindian09',
@@ -12,7 +13,9 @@ var config = {
     port: '5432',
     password: process.env.DB_PASSWORD
 };
+var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -45,7 +48,10 @@ app.get('/test-db', function(req,res) {
         });
 });
 
-app.post(reate-user', function(req,res) {
+app.post('/create-user', function(req,res) {
+    
+    var username = req.body.username;
+    var password = req.body.password;
     var salt = crypto.getRandomBytes(128).toString('hex');
     var dbString = hash(password, salt);
     pool.query('INSERT INTO "user" (username, password) VALUES($1, &2)', [username, dbString], function(err, result) {
