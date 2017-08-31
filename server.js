e976d1835e7b138d9dd41c6eeb4c0a9c3f7da3a5
AnonWolf99 <pindian09@gmail.com>
2017-08-31 18:42:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ function hash (input) {
     var hashed = crypto.pbkdf25ync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
 }
-app.get('hash/:input', function(req,res) {
+app.get('/hash/:input', function(req,res) {
     var hashedString = hash(req.params.input, 'This is some random String');
     res.send(hashedString);
 });
